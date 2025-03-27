a50fea95fe7cd5b228bacf146227810260b37d75
gaurav <gsood07@gmail.com>
2025-03-26 22:11:36 -0700

    Update __init__.py

--- reporoulette/samplers/__init__.py
+++ reporoulette/samplers/__init__.py
@@ -2,5 +2,6 @@
 from .id_sampler import IDSampler
 from .temporal_sampler import TemporalSampler
 from .bigquery_sampler import BigQuerySampler
+from .bigquery_sampler import GHArchiveSampler
 
 __all__ = ['IDSampler', 'TemporalSampler', 'BigQuerySampler']
